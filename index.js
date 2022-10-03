18e8040ede29deccafd427899226a4f0e0e00d28
Limon714 <limon4u714@gmail.com>
2022-10-03 23:05:30 +0600

    socket-app

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const expressServer = http.createServer(app);
+
+const { Server } = require('socket.io');
+const io = new Server(expressServer);
+
+io.on('connection', function (socket) {
+    console.log("New user connected")
+
+    socket.on('disconnect', function () {
+        console.log("Disconnected")
+    })
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+})
+
+expressServer.listen(3000, function () {
+    console.log("server run at 3000 port");
+})
